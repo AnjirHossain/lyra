7c7709288bc1911f83c45c8f7fb34ff1b5be63dd
AnjirHossain <anjirh@uw.edu>
2017-11-29 18:36:44 -0800

    .

--- server.prod.js
+++ server.prod.js
@@ -4,26 +4,32 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
-console.log('__dirname: ', __dirname);
-
 app.use('/public', express.static(__dirname + '/build'));
 
 app.get('*.css', function(req, res) {
+  console.log('Request for css: ', req);
+
   res.set('Contenttype', 'text/css');
   res.sendFile(req.originalUrl);
 });
 
 app.get('*.js', function(req, res) {
+  console.log('Request for js: ', req);
+
   res.set('Contenttype', 'application/javascript');
   res.sendFile(req.originalUrl);
 });
 
 app.get('*.json', function(req, res) {
+  console.log('Request for json: ', req);
+
   res.set('Contenttype', 'application/json');
   res.sendFile(req.originalUrl);
 });
 
 app.get('*', function(req, res) {
+  console.log('Request for everything else: ', req);
+
   res.set('Contenttype', 'text/html');
   res.sendFile(__dirname + '/build/index.html');
 });
