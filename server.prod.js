1d493aebc0af3b7a88c74dcec471e42e44193200
AnjirHossain <anjirh@uw.edu>
2017-11-30 00:28:42 -0800

    Heroku auto-deploy

--- server.prod.js
+++ server.prod.js
@@ -6,11 +6,26 @@ var port = process.env.PORT || 3000;
 
 app.use('/public', express.static(__dirname + '/build'));
 
-app.get('/', function(req, res) {
+app.get('*.css', function(req, res) {
+  res.set('Contenttype', 'text/css');
+  res.sendFile(req.originalUrl);
+});
+
+app.get('*.js', function(req, res) {
+  res.set('Contenttype', 'application/javascript');
+  res.sendFile(req.originalUrl);
+});
+
+app.get('*.json', function(req, res) {
+  res.set('Contenttype', 'application/json');
+  res.sendFile(req.originalUrl);
+});
+
+app.get('*', function(req, res) {
   res.set('Contenttype', 'text/html');
-  res.sendFile(__dirname + '/build/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(port, function() {
-  console.log('Lyra 2 demo coming soon!');
+  console.log('Lyra 2 demo coming soon!')
 });
